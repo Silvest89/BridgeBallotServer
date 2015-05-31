9efda0f69146cd02a18c10a86ef910e93d11470b
Johnnie Ho <johnnieho89@gmail.com>
2015-05-31 19:46:39 +0200

    * Updated network code

--- src/bridgeballotserver/BridgeBallotServer.java
+++ src/bridgeballotserver/BridgeBallotServer.java
@@ -12,49 +12,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class BridgeBallotServer extends Thread {
-
-    private ServerSocket serverSocket;
-    private int port;
-    private boolean running = false;
-
-    public BridgeBallotServer(int port) {
-        this.port = port;
-    }
-
-    public void startServer() {
-        try {
-            serverSocket = new ServerSocket(port);
-            this.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void stopServer() {
-        running = false;
-        this.interrupt();
-    }
-
-    @Override
-    public void run() {
-        running = true;
-        Database.getDataSource();
-        while (running) {
-            try {
-                System.out.println("Listening for a connection");
-
-                // Call accept() to receive the next connection
-                Socket socket = serverSocket.accept();
-
-                // Pass the socket to the RequestHandler thread for processing
-                RequestHandler requestHandler = new RequestHandler(socket);
-                requestHandler.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+public class BridgeBallotServer {
 
     public static void main(String[] args) {
         /*
@@ -64,9 +22,8 @@ public class BridgeBallotServer extends Thread {
          }
          */
         int port = 21;
-        System.out.println("Start server on port: " + port);
 
-        BridgeBallotServer server = new BridgeBallotServer(port);
+        Protocol server = new Protocol(port);
         server.startServer();
 
         // Automatically shutdown in 1 minute
@@ -80,65 +37,4 @@ public class BridgeBallotServer extends Thread {
          server.stopServer();
          */
     }
-}
-
-class RequestHandler extends Thread {
-
-    private Socket socket;
-
-    RequestHandler(Socket socket) {
-        this.socket = socket;
-    }
-
-    @Override
-    public void run() {
-        try {
-            System.out.println("Received a connection");
-
-            // Get input and output streams
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-
-            // Write out our header to the client
-            out.println("Echo Server 1.0");
-            out.flush();
-
-            String line = in.readLine();
-
-            switch (line) {
-                case "LOGIN":
-                    String username = "";
-                    String password = "";
-                    int counter = 0;
-                    while ((line = in.readLine()) != null) {
-                        if (counter == 0) {
-                            username = line;
-                        } else if (counter == 1) {
-                            password = line;
-                        }
-                        counter++;
-                    }
-
-                    boolean correctLogin = new Database().validateLogin(username, password);
-
-                    if (correctLogin) {
-                        System.out.println("Access granted");
-                    } else {
-                        System.out.println("Access refused");
-                    }
-                break;
-
-            }
-
-            // Close our connection
-            in.close();
-            out.close();
-            socket.close();
-
-            System.out.println("Connection closed");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 }
---
+++ src/bridgeballotserver/Protocol.java
@@ -0,0 +1,119 @@
+package bridgeballotserver;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Johnnie Ho on 31-5-2015.
+ */
+public class Protocol extends Thread {
+
+    private ServerSocket serverSocket;
+    private int port;
+    private boolean running = false;
+
+    public Protocol(int port){
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        Database.getDataSource();
+        while (running) {
+            try {
+                System.out.println("Listening for a connection");
+
+                // Call accept() to receive the next connection
+                Socket socket = serverSocket.accept();
+
+                // Pass the socket to the RequestHandler thread for processing
+                RequestHandler requestHandler = new RequestHandler(socket);
+                requestHandler.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void startServer() {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Starting server on port: " + port);
+            this.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopServer() {
+        running = false;
+        this.interrupt();
+    }
+}
+
+class RequestHandler extends Thread {
+
+    public final static class MessageType {
+        public static final int LOGIN = 0;
+        public static final int ADD_BRIDGE = 1;
+        public static final int CLOSE_CONNECTION = 2;
+    }
+
+    public final static class ReturnType {
+        public static final int SUCCESS = 0;
+        public static final int FAILURE = 1;
+    }
+
+    private Socket socket;
+
+    RequestHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+            try {
+                System.out.println("Received a connection");
+                // Get input and output streams
+                //ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                out.flush();
+                int line = in.readInt();
+                System.out.println("Test1:" + line);
+                switch (line) {
+                    case MessageType.LOGIN: {
+                        parseLogin(in, out);
+                        break;
+                    }
+
+                }
+
+                // Close our connection
+                in.close();
+                out.close();
+                //socket.close();
+
+                System.out.println("Connection closed");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    public void parseLogin(ObjectInputStream in, ObjectOutputStream out) throws Exception{
+        String[] loginDetails = new String[2];
+        loginDetails = (String[]) in.readObject();
+        System.out.println(loginDetails);
+        boolean correctLogin = new Database().validateLogin(loginDetails[0], loginDetails[1]);
+
+        if (correctLogin) {
+            out.writeInt(ReturnType.SUCCESS);
+        } else {
+            out.writeInt(ReturnType.FAILURE);
+        }
+        out.flush();
+
+    }
+}
